0330101a6f289cb3eddffdc98111aa92279bed67
caesar <caesar.lee@mirrormedia.mg>
2022-10-28 16:13:41 +0800

    refactor: remove server side stylesheet

--- packages/nextjs-13.0.0-template/pages/_document.js
+++ packages/nextjs-13.0.0-template/pages/_document.js
@@ -1,7 +1,6 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-function CustomDocument() {
+export default function CustomDocument() {
   return (
     <Html>
       <Head>
@@ -17,29 +16,3 @@ function CustomDocument() {
     </Html>
   )
 }
-
-CustomDocument.getInitialProps = async (ctx) => {
-  const sheet = new ServerStyleSheet()
-  const originalRenderPage = ctx.renderPage
-
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-      })
-    const initialProps = await Document.getInitialProps(ctx)
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      ),
-    }
-  } finally {
-    sheet.seal()
-  }
-}
-
-export default CustomDocument
