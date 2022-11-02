3bba75a5f5eb7f663812f84e7193fbf315f47ec1
caesar <caesar.lee@mirrormedia.mg>
2022-11-02 11:03:52 +0800

    fix(election-map): handle no node situation

--- packages/election-map/hook/useElementDimension.js
+++ packages/election-map/hook/useElementDimension.js
@@ -3,22 +3,23 @@ import { useCallback, useState } from 'react'
 export const useElementDimension = () => {
   const [dimension, setDimension] = useState(null)
   const elementRef = useCallback((node) => {
-    if (!node) console.error('there is no elementRef!', node)
-    const { width, height } = node.getBoundingClientRect()
-    setDimension({
-      width,
-      height,
-    })
-
-    const resizeObserver = new ResizeObserver(() => {
+    if (node) {
       const { width, height } = node.getBoundingClientRect()
       setDimension({
         width,
         height,
       })
-    })
 
-    resizeObserver.observe(node)
+      const resizeObserver = new ResizeObserver(() => {
+        const { width, height } = node.getBoundingClientRect()
+        setDimension({
+          width,
+          height,
+        })
+      })
+
+      resizeObserver.observe(node)
+    }
   }, [])
 
   return { elementRef, dimension }
