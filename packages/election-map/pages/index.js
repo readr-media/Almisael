ebd57ce9e5b2d8a0f2603b9077ff7e350fa90abf
caesar <caesar.lee@mirrormedia.mg>
2022-11-15 20:28:08 +0800

    feat(election-map): add nav

--- packages/election-map/pages/index.js
+++ packages/election-map/pages/index.js
@@ -1,9 +1,11 @@
 import { Dashboard } from '../components/Dashboard'
 import { LandingPage } from '../components/LandingPage'
+import { NavBar } from '../components/NavBar'
 
 export default function Home() {
   return (
     <>
+      <NavBar />
       <LandingPage />
       <Dashboard />
     </>
