03a199edd49663e7c98579cb7e5458e6c541289d
caesar <caesar.lee@mirrormedia.mg>
2022-11-16 15:21:47 +0800

    feat(election-map): add subtype radio

---
+++ packages/election-map/components/ElectionRadio.js
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
+const mockSelectedType = '區域'
+const mockSubTypes = ['區域', '原住民']
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const Label = styled.label`
+  display: inline-flex;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+  font-size: 24px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  &:first-of-type {
+    margin-right: 5px;
+  }
+  & span:after {
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    background: #000;
+  }
+`
+
+const Input = styled.input`
+  position: absolute;
+  opacity: 0;
+  height: 0;
+  width: 0;
+  &:checked ~ span:after {
+    display: block;
+  }
+`
+
+const Radio = styled.span`
+  height: 24px;
+  width: 24px;
+  margin-right: 5px;
+  background-color: #fff;
+  border: 1px solid #000;
+  border-radius: 50%;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  &:after {
+    content: '';
+    display: none;
+  }
+`
+
+export const ElectionRadio = ({
+  className,
+  // selectedType = mockSelectedType,
+  // subTypes = mockSubTypes,
+}) => {
+  const [selectedType, setSelectedType] = useState(mockSelectedType)
+  const subTypes = mockSubTypes
+
+  return (
+    <Wrapper className={className}>
+      {subTypes.map((subType) => (
+        <Label
+          key={subType}
+          onClick={() => {
+            setSelectedType(subType)
+          }}
+        >
+          <Input type="radio" checked={subType === selectedType} />
+          <Radio />
+          {subType}
+        </Label>
+      ))}
+    </Wrapper>
+  )
+}
