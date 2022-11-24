8459d6ed741bb74ba2b17f97fe7b70f1d61d8512
caesar <caesar.lee@mirrormedia.mg>
2022-11-24 23:26:08 +0800

    chore(election-map): add liveblog ga

--- packages/election-map/components/LiveblogContainer.js
+++ packages/election-map/components/LiveblogContainer.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
+import ReactGA from 'react-ga'
 
 import { electionMapColor } from '../consts/colors'
 import lb from '@readr-media/react-live-blog'
@@ -13,6 +14,30 @@ const LiveBlogWrapper = styled.div`
 
 export const LiveblogContainer = () => {
   const [initialLiveblog, setInitialLiveblog] = useState(false)
+
+  const handleLiveblogEvent = (event) => {
+    const { eventName, eventTarget, eventValue, metadata } = event
+    console.log(event)
+    if (eventTarget === '繼續閱讀按鈕') {
+      ReactGA.event({
+        category: 'Projects',
+        action: eventName,
+        label: `liveblog item ${eventValue} ${metadata.article.title}`,
+      })
+    } else if (eventTarget === 'tag 按鈕') {
+      ReactGA.event({
+        category: 'Projects',
+        action: eventName,
+        label: `tag： ${eventValue}`,
+      })
+    } else if (eventTarget === '外連按鈕') {
+      ReactGA.event({
+        category: 'Projects',
+        action: eventName,
+        label: `liveblog item ${eventTarget} ${metadata.article.title}`,
+      })
+    }
+  }
 
   useEffect(() => {
     const fetchLiveblog = async () => {
@@ -32,6 +57,7 @@ export const LiveblogContainer = () => {
           fetchLiveblogUrl="https://editools-gcs.readr.tw/files/liveblogs/election2022.json"
           fetchImageBaseUrl="https://editools-gcs.readr.tw"
           toLoadPeriodically={true}
+          onChange={handleLiveblogEvent}
         />
       )}
     </LiveBlogWrapper>
