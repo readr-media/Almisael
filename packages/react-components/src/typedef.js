308964c937b81cef211520fd33d3260bac36153d
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2023-03-01 13:25:09 +0800

    docs(react-components): add typedef

---
+++ packages/react-components/src/typedef.js
@@ -0,0 +1,12 @@
+export default {}
+
+/**
+ *  @typedef {Object}            Post
+ *  @property {string}           caption
+ *  @property {number | string}  [id]
+ *  @property {string}           [imageUrl]
+ *  @property {string}           [alt]
+ *  @property {string}           [link]
+ *  @property {string}           [date]
+ *  @property {number}           [time]
+ */
