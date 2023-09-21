75b8a148379d6e33202621abe33e5144c10eec79
bibibobobi <smilingcat33@gmail.com>
2023-09-21 12:03:36 +0800

    add donate-btn-header-rect component

---
+++ packages/react-components/src/donate-btn-header-rect/index.js
@@ -0,0 +1,55 @@
+import NextLink from 'next/link'
+import styled from 'styled-components'
+
+const DonateLink = styled(NextLink)`
+  font-family: 'Noto Sans TC', sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  font-weight: 700;
+  letter-spacing: 2.5px;
+  color: #000928;
+  background-color: #fff;
+  border: 1px solid #000928;
+  border-radius: 2px;
+  height: 40px;
+  width: 104px;
+
+  &:hover,
+  &:active,
+  &:focus {
+    background-color: #ebf02c;
+  }
+`
+
+/**
+ * @param {Object} props
+ * @param {string} [props.href='https://www.readr.tw/donate']
+ * @param {string} [props.title='贊助我們']
+ * @param {boolean} [props.openNewTab=true]
+ * @param {string} [props.className='readr-donate-button']
+ * @param {import("react").MouseEventHandler} [props.onClick]
+ * @return {JSX.Element}
+ */
+
+export default function DonateBtnRect({
+  href = 'https://www.readr.tw/donate',
+  title = '贊助我們',
+  openNewTab = true,
+  className = 'readr-donate-button',
+  onClick,
+}) {
+  return (
+    <DonateLink
+      href={href}
+      target={openNewTab ? '_blank' : '_self'}
+      rel="external nofollow"
+      onClick={onClick}
+      className={className}
+      aria-label="前往 READr 贊助頁面"
+    >
+      {title}
+    </DonateLink>
+  )
+}
