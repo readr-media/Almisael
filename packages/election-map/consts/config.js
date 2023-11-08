f179c9a045664d741f3855f9a79460ea9a58809c
caesar <caesar.lee@mirrormedia.mg>
2023-11-08 16:34:01 +0800

    chore(election-map): update default organization

--- packages/election-map/consts/config.js
+++ packages/election-map/consts/config.js
@@ -1,3 +1,8 @@
-export const organization = 'mirror-media'
-// export const organization = 'readr-media'
+// export const organization = 'mirror-media'
+export const organization = 'readr-media'
 export const environment = 'prod' // 'dev | prod'
+
+/*
+gsutil -m cp -r -a public-read ./out/* gs://statics.mirrormedia.mg/projects/election2022
+gsutil -m cp -r -a public-read ./out/* gs://readr-coverages/3/election2022
+*/
