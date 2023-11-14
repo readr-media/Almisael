51d1494d22b36080dd63d673a098312de46808b7
caesar <caesar.lee@mirrormedia.mg>
2023-11-14 13:15:51 +0800

    refactor(election-map): update mock data for every election

--- packages/election-map/mock-datas/maps/presidents/town/63000010.js
+++ packages/election-map/mock-datas/maps/presidents/town/63000010.js
@@ -1,4 +1,8 @@
-export const mockData = {
+// [地圖資料] 總統副總統 鄉鎮市區層級資料
+export const data = {
+  updatedAt: '2022-11-11 12:09:25',
+  is_started: true,
+  is_running: false,
   districts: [
     {
       range: '台北市 松山區 精忠里',
@@ -8,16 +12,16 @@ export const mockData = {
       profRate: 80.0,
       candidates: [
         {
-          canNo: 1,
-          name: '蔡英文',
-          party: '民進黨',
+          canNo: '1',
+          name: '蔡英文 賴清德',
+          party: '民主進步黨',
           tksRate: 57.13,
           candVictor: '*',
         },
         {
-          canNo: 2,
-          name: '韓國瑜',
-          party: '國民黨',
+          canNo: '2',
+          name: '韓國瑜 張善政',
+          party: '中國國民黨',
           tksRate: 78.61,
           candVictor: ' ',
         },
@@ -31,16 +35,16 @@ export const mockData = {
       profRate: 80.0,
       candidates: [
         {
-          canNo: 1,
-          name: '蔡英文',
-          party: '民進黨',
+          canNo: '1',
+          name: '蔡英文 賴清德',
+          party: '民主進步黨',
           tksRate: 57.13,
           candVictor: '*',
         },
         {
-          canNo: 2,
-          name: '韓國瑜',
-          party: '國民黨',
+          canNo: '2',
+          name: '韓國瑜 張善政',
+          party: '中國國民黨',
           tksRate: 38.61,
           candVictor: ' ',
         },
@@ -54,16 +58,16 @@ export const mockData = {
       profRate: 80.0,
       candidates: [
         {
-          canNo: 1,
-          name: '蔡英文',
-          party: '民進黨',
+          canNo: '1',
+          name: '蔡英文 賴清德',
+          party: '民主進步黨',
           tksRate: 57.13,
           candVictor: '*',
         },
         {
-          canNo: 2,
-          name: '韓國瑜',
-          party: '國民黨',
+          canNo: '2',
+          name: '韓國瑜 張善政',
+          party: '中國國民黨',
           tksRate: 88.61,
           candVictor: ' ',
         },
