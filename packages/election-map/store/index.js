565770365ad7976d7501a8d2d5b762e0689735cd
DianYangFu <dianyangfu@gmail.com>
2023-11-30 16:12:17 +0800

    feat(election-map): extend redux hooks, add types

--- packages/election-map/store/index.js
+++ packages/election-map/store/index.js
@@ -1,6 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import electionReducer from './election-slice'
 
+/**
+ * @typedef {ReturnType<typeof store.getState>} RootState
+ */
+
+/**
+ * @typedef  {typeof store.dispatch} AppDispatch
+ */
+
 const store = configureStore({
   reducer: { election: electionReducer },
 })
