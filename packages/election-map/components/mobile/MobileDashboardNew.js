35181e93f991ccef8089186d83f5d381e459a936
DianYangFu <dianyangfu@gmail.com>
2023-12-04 17:45:27 +0800

    feat(election-map/hooks): add useDistrictMapping to get data

--- packages/election-map/components/mobile/MobileDashboardNew.js
+++ packages/election-map/components/mobile/MobileDashboardNew.js
@@ -1,168 +1,15 @@
-import React, { useState, useEffect, Fragment } from 'react'
-import { districtCode } from '../../mock-datas/districtCode'
+import { useState, useEffect, Fragment } from 'react'
 import { electionNamePairs } from '../../utils/election'
 import Selector from './Selector'
 import ElectionSelector from './ElectionSelector'
 import styled from 'styled-components'
 import InfoBox from './InfoBox'
 import YearComparisonMenuBar from './YearComparisonMenuBar'
-/**
- * @typedef {import('../../store/index').RootState}RootState
- */
+import { useDistrictMapping } from '../../hook/useDistrictMapping'
 
 import { useAppSelector } from '../../hook/useRedux'
 import { useAppDispatch } from '../../hook/useRedux'
 import { electionActions } from '../../store/election-slice'
-
-// const ELECTION_TYPE = [
-//   {
-//     electionType: 'president',
-//     electionName: '正副總統',
-//     years: [{ key: 2012 }, { key: 2016 }, { key: 2020 }, { key: 2024 }],
-//   },
-//   {
-//     electionType: 'mayor',
-//     electionName: '縣市首長',
-//     years: [{ key: 2010 }, { key: 2014 }, { key: 2018 }, { key: 2022 }],
-//   },
-//   {
-//     electionType: 'legislator',
-//     electionName: '立法委員',
-//     years: [{ key: 2012 }, { key: 2016 }, { key: 2020 }, { key: 2024 }],
-//   },
-//   {
-//     electionType: 'councilMember',
-//     electionName: '縣市議員',
-//     years: [{ key: 2010 }, { key: 2014 }, { key: 2018 }, { key: 2022 }],
-//     subtypes: [
-//       { electionName: '區域', electionType: 'normal' },
-//       { electionName: '原住民', electionType: 'indigenous' },
-//     ],
-//   },
-//   {
-//     electionType: 'referendum',
-//     electionName: '全國性公民投票',
-//     years: [
-//       {
-//         key: 2018,
-//         numbers: [
-//           {
-//             year: 2018,
-//             key: '7',
-//             name: '公投第7案',
-//             detail:
-//               '你是否同意以「平均每年至少降低1%」之方式逐年降低火力發電廠發電量?',
-//           },
-//           {
-//             year: 2018,
-//             key: '8',
-//             name: '公投第8案',
-//             detail:
-//               '您是否同意確立「停止新建、擴建任何燃煤發電廠或發電機組(包括深澳電廠擴建)」之能源政策?',
-//           },
-//           {
-//             year: 2018,
-//             key: '9',
-//             name: '公投第9案',
-//             detail:
-//               '你是否同意政府維持禁止開放日本福島311核災相關地區，包括福島與周遭4縣市(茨城、櫪木、群馬、千葉)等地區農產品及食品進口?',
-//           },
-//           {
-//             year: 2018,
-//             key: '10',
-//             name: '公投第10案',
-//             detail: '你是否同意民法婚姻規定應限定在一男一女的結合?',
-//           },
-//           {
-//             year: 2018,
-//             key: '11',
-//             name: '公投第11案',
-//             detail:
-//               '你是否同意在國民教育階段內(國中及國小)，教育部及各級學校不應對學生實施性別平等教育法施行細則所定之同志教育?',
-//           },
-//           {
-//             year: 2018,
-//             key: '12',
-//             name: '公投第12案',
-//             detail:
-//               '你是否同意以民法婚姻規定以外之其他形式來保障同性別二人經營永久共同生活的權益?',
-//           },
-//           {
-//             year: 2018,
-//             key: '13',
-//             name: '公投第13案',
-//             detail:
-//               '你是否同意，以「台灣」(Taiwan)為全名申請參加所有國際運動賽事及2020年東京奧運?',
-//           },
-//           {
-//             year: 2018,
-//             key: '14',
-//             name: '公投第14案',
-//             detail: '您是否同意，以民法婚姻章保障同性別二人建立婚姻關係?',
-//           },
-//           {
-//             year: 2018,
-//             key: '15',
-//             name: '公投第15案',
-//             detail:
-//               '您是否同意，以「性別平等教育法」明定在國民教育各階段內實施性別平等教育，且內容應涵蓋情感教育、性教育、同志教育等課程?',
-//           },
-//           {
-//             year: 2018,
-//             key: '16',
-//             name: '公投第16案',
-//             detail:
-//               '廢除電業法第95條第1項，即廢除「核能發電設備應於中華民國一百十四年以前，全部停止運轉」之條文?',
-//           },
-//         ],
-//       },
-//       {
-//         key: 2021,
-//         numbers: [
-//           {
-//             year: 2021,
-//             key: '17',
-//             name: '公投第17案',
-//             detail: '您是否同意核四啟封商轉發電？',
-//           },
-//           {
-//             year: 2021,
-//             key: '18',
-//             name: '公投第18案',
-//             detail:
-//               '你是否同意政府應全面禁止進口含有萊克多巴胺之乙型受體素豬隻之肉品、內臟及其相關產製品？',
-//           },
-//           {
-//             year: 2021,
-//             key: '19',
-//             name: '公投第19案',
-//             detail:
-//               '你是否同意公民投票案公告成立後半年內，若該期間內遇有全國性選舉時，在符合公民投票法規定之情形下，公民投票應與該選舉同日舉行？',
-//           },
-//           {
-//             year: 2021,
-//             key: '20',
-//             name: '公投第20案',
-//             detail:
-//               '您是否同意中油第三天然氣接收站遷離桃園大潭藻礁海岸及海域？（即北起觀音溪出海口，南至新屋溪出海口之海岸，及由上述海岸最低潮線往外平行延伸五公里之海域）',
-//           },
-//         ],
-//       },
-//       {
-//         key: 2022,
-//         numbers: [
-//           {
-//             year: 2022,
-//             key: 'F1',
-//             name: '憲法修正案第1案',
-//             detail:
-//               '中華民國國民年滿十八歲者，有依法選舉、罷免、創制、複決及參加公民投票之權。',
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ]
 
 /**
  * @typedef {Object} NationData
@@ -231,7 +78,7 @@ const Wrapper = styled.div`
   background-color: ${
     /**
      * @param {Object} props
-     * @param {boolean} props.isCompareMode
+     * @param {boolean} [props.isCompareMode]
      */
     ({ isCompareMode }) => (isCompareMode ? '#E9E9E9' : 'transparent')
   };
@@ -262,6 +109,7 @@ const YearWrapper = styled(TopButton)`
  * Dashboard for new election map, created in 2023.11.20
  */
 export const MobileDashboardNew = () => {
+  const { districtMapping, hasDistrictMapping } = useDistrictMapping()
   // const [currentElection, setCurrentElection] = useState(ELECTION_TYPE[0])
   const dispatch = useAppDispatch()
   const { stopCompare, changeLevelControl, changeYear, resetLevelControl } =
@@ -284,7 +132,7 @@ export const MobileDashboardNew = () => {
   const [currentOpenSelector, setCurrentOpenSelector] = useState('')
 
   /** @type {CountyData[]} */
-  const allCounty = districtCode.sub
+  const allCounty = districtMapping.sub
 
   /** @type {TownData[]} */
   const allTown = getAllTown(currentCountyCode)
@@ -370,7 +218,7 @@ export const MobileDashboardNew = () => {
   function getCurrentDistrictCode() {
     switch (currentDistrictType) {
       case 'nation':
-        return districtCode.code
+        return districtMapping.code
       case 'county':
         return currentCountyCode
       case 'town':
@@ -379,7 +227,7 @@ export const MobileDashboardNew = () => {
         return currentVillageCode
 
       default:
-        return districtCode.code
+        return districtMapping.code
     }
   }
 
@@ -404,11 +252,11 @@ export const MobileDashboardNew = () => {
   }
   const nationAndAllCounty = [
     {
-      type: districtCode.type,
-      code: districtCode.code,
-      name: districtCode.name,
+      type: districtMapping.type,
+      code: districtMapping.code,
+      name: districtMapping.name,
     },
-    ...districtCode.sub,
+    ...districtMapping.sub,
   ]
   /**
    *
@@ -416,8 +264,8 @@ export const MobileDashboardNew = () => {
    * @param {string} code
    */
   const handleOnClick = (
-    type = districtCode.type,
-    code = districtCode.code
+    type = districtMapping.type,
+    code = districtMapping.code
   ) => {
     setCurrentDistrictType(type)
     switch (type) {
@@ -494,6 +342,10 @@ export const MobileDashboardNew = () => {
       </TopButton>
     </>
   )
+
+  if (!hasDistrictMapping) {
+    return <Wrapper>loading....</Wrapper>
+  }
   return (
     <>
       {shouldOpenYearComparisonMenuBar && (
---
+++ packages/election-map/hook/useDistrictMapping.js
@@ -0,0 +1,95 @@
+import { useState, useMemo, useEffect } from 'react'
+import axios from 'axios'
+
+const fetchJson = async (url) => {
+  const result = await axios.get(url)
+  return result
+}
+const fetchDistrictJson = async () => {
+  const districtCodeJson =
+    'https://whoareyou-gcs.readr.tw/elections-dev/district-mapping/district/mapping.json'
+
+  try {
+    const responses = await Promise.allSettled([fetchJson(districtCodeJson)])
+
+    const responseJson = responses.map((response, i) => {
+      if (response.status === 'fulfilled') {
+        return response.value
+      } else if (response.status === 'rejected') {
+        throw new Error(
+          `Fetch ${i} level map topojson failed: ${response.reason}`
+        )
+      }
+    })
+    console.log(responseJson)
+    return responseJson
+  } catch (error) {
+    console.error('fetch map error', error)
+  }
+}
+
+/**
+ * @typedef {Object} NationData
+ * @property {string} name
+ * @property {string} code
+ * @property {'nation'} type
+ * @property {CountyData[]} sub
+ */
+
+/**
+ * @typedef {Object} CountyData
+ * @property {string} name
+ * @property {string} code
+ * @property {'county'} type
+ * @property {TownData[]} sub
+ */
+/**
+ * @typedef {Object} TownData
+ * @property {string} name
+ * @property {string} code
+ * @property {'town'} type
+ * @property {VillageData[]} sub
+ */
+/**
+ * @typedef {Object} VillageData
+ * @property {string} name
+ * @property {string} code
+ * @property {'village'} type
+ * @property {null} sub
+ */
+
+/**
+ * @typedef {'nation' | 'county' | 'town' | 'village'} DistrictType
+ */
+
+/**
+ * @type {NationData}
+ */
+const initialDistrictMapping = {
+  code: '',
+  name: '台灣',
+  type: 'nation',
+  sub: [],
+}
+
+/**
+ * Achieve districtCode.
+ */
+export const useDistrictMapping = () => {
+  const [districtMapping, setDistrictMapping] = useState(initialDistrictMapping)
+
+  const hasDistrictMapping = useMemo(() => {
+    return districtMapping.sub.length > 0
+  }, [districtMapping])
+
+  useEffect(() => {
+    const prepareDistrictMapping = async () => {
+      const responses = await fetchDistrictJson()
+      const [districtMapping] = responses
+      setDistrictMapping(districtMapping.data)
+    }
+    prepareDistrictMapping()
+  }, [])
+
+  return { districtMapping, hasDistrictMapping }
+}
