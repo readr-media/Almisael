c7282b860444e88575c5dab115e3d9a010d6a743
Xuan <as840922qq@gmail.com>
2023-12-04 11:05:55 +0800

    docs(vote): add `president/legislator` mock data

---
+++ packages/election-map/mock-datas/votes-comparison/legislator/party/all.js
@@ -0,0 +1,45 @@
+export const data = {
+  updatedAt: '2024-11-20 13:11:22',
+  year: '2024',
+  type: 'legislator-party',
+  version: 'v2',
+  title: '立法委員選舉（不分區）',
+  parties: [
+    {
+      candNo: '1',
+      party: {
+        label: '中國國民黨',
+        href: '',
+        imgSrc: '',
+      },
+
+      tks: 3813365,
+      tksRate: 31.04,
+      seats: 1,
+    },
+    {
+      candNo: '2',
+      party: {
+        label: '民主進步黨',
+        href: '',
+        imgSrc: '',
+      },
+
+      tks: 3813365,
+      tksRate: 31.04,
+      seats: 5,
+    },
+    {
+      candNo: '3',
+      party: {
+        label: '親民黨',
+        href: '',
+        imgSrc: '',
+      },
+
+      tks: 3813365,
+      tksRate: 31.04,
+      seats: 10,
+    },
+  ],
+}
