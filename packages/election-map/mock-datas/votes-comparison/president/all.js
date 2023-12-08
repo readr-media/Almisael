3ab1988d6c7dc547da6e0863f6d12677559dba03
Xuan <as840922qq@gmail.com>
2023-12-08 13:33:54 +0800

    docs(map/votes): update mock data

--- packages/election-map/mock-datas/votes-comparison/president/all.js
+++ packages/election-map/mock-datas/votes-comparison/president/all.js
@@ -1,75 +1,22 @@
 export const data = {
-  updatedAt: '2022-11-24 13:11:22',
-  year: '2016',
+  updateAt: '2023-12-05 17:05:00',
+  year: '2020',
   type: 'president',
-  version: 'v2',
   title: '正副總統選舉',
+  version: 'v2',
   candidates: [
     {
-      candNo: '1',
-      names: [
-        {
-          label: '朱立倫',
-          href: 'https://whoareyou.readr.tw/person/135',
-          imgSrc:
-            'http://www.ly.gov.tw/upload/01_introduce/0103_leg/LGpicture/ly1000_4_00016_2f.jpg',
-        },
-        {
-          label: '王如玄',
-          href: 'https://whoareyou.readr.tw/person/36706',
-          imgSrc: '',
-        },
-      ],
-      parties: [
-        {
-          label: '中國國民黨',
-          href: '',
-          imgSrc: '',
-        },
-      ],
-      tks: 3813365,
-      tksRate: 31.04,
-      candVictor: false,
-    },
-    {
-      candNo: '2',
-      names: [
-        {
-          label: '蔡英文',
-          href: 'https://whoareyou.readr.tw/person/636',
-          imgSrc:
-            'http://www.ly.gov.tw/upload/01_introduce/0103_leg/LGpicture/ly1000_6_00192_13f.jpg',
-        },
-        {
-          label: '陳建仁',
-          href: 'https://whoareyou.readr.tw/person/36707',
-          imgSrc: '',
-        },
-      ],
-      parties: [
-        {
-          label: '民主進步黨',
-          href: '',
-          imgSrc: '',
-        },
-      ],
-      tks: 6894744,
-      tksRate: 56.12,
-      candVictor: true,
-    },
-    {
-      candNo: '3',
+      candNo: 1,
       names: [
         {
           label: '宋楚瑜',
           href: 'https://whoareyou.readr.tw/person/36703',
-          imgSrc: '',
+          imgSrc: null,
         },
         {
-          label: '徐欣瑩',
-          href: 'https://whoareyou.readr.tw/person/399',
-          imgSrc:
-            'http://www.ly.gov.tw/upload/01_introduce/0103_leg/LGpicture/80047.jpg',
+          label: '余湘',
+          href: 'https://whoareyou.readr.tw/person/36704',
+          imgSrc: null,
         },
       ],
       parties: [
@@ -79,9 +26,61 @@ export const data = {
           imgSrc: '',
         },
       ],
-      tks: 1576861,
-      tksRate: 12.83,
+      tks: 608590,
+      tksRate: 4.26,
       candVictor: false,
+    },
+    {
+      candNo: 2,
+      names: [
+        {
+          label: '韓國瑜',
+          href: 'https://whoareyou.readr.tw/person/121',
+          imgSrc: null,
+        },
+        {
+          label: '張善政',
+          href: 'https://whoareyou.readr.tw/person/36705',
+          imgSrc: 'https://whoareyou-gcs.readr.tw/person/張善政.jpg',
+        },
+      ],
+      parties: [
+        {
+          label: '中國國民黨',
+          href: '',
+          imgSrc: '',
+        },
+      ],
+      tks: 5522119,
+      tksRate: 38.61,
+      candVictor: false,
+    },
+    {
+      candNo: 3,
+      names: [
+        {
+          label: '蔡英文',
+          href: 'https://whoareyou.readr.tw/person/636',
+          imgSrc:
+            'http://www.ly.gov.tw/upload/01_introduce/0103_leg/LGpicture/ly1000_6_00192_13f.jpg',
+        },
+        {
+          label: '賴清德',
+          href: 'https://whoareyou.readr.tw/person/226',
+          imgSrc:
+            'http://www.ly.gov.tw/upload/01_introduce/0103_leg/LGpicture/71105.jpg',
+        },
+      ],
+      parties: [
+        {
+          label: '民主進步黨',
+          href: '',
+          imgSrc: '',
+        },
+      ],
+      tks: 8170231,
+      tksRate: 57.13,
+      candVictor: true,
     },
   ],
 }
