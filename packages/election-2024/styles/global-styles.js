88eafdde4f057eb5410ee55ef6b45c0f76068577
DianYangFu <dianyangfu@gmail.com>
2023-12-21 14:21:06 +0800

    style(election-2024): update global-style

--- packages/election-2024/styles/global-styles.js
+++ packages/election-2024/styles/global-styles.js
@@ -22,4 +22,28 @@ export const GlobalStyles = createGlobalStyle`
     cursor: pointer;
     outline: inherit;  
   }
+
+  blockquote,
+  dl,
+  dd,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  hr,
+  figure,
+  p,
+  pre {
+    margin: 0;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
 `
