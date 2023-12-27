f307b902255231b6cdcb7182dd41250cae35ecef
DianYangFu <dianyangfu@gmail.com>
2023-12-27 11:04:59 +0800

    chore(election-2024): add build and export setting

--- packages/election-2024/config/index.mjs
+++ packages/election-2024/config/index.mjs
@@ -4,11 +4,30 @@ const projectName = 'dev-election2024-homepage-1222-2'
 let assetPrefixPath = ''
 let jsonEndpoint = ''
 let staticFileDestination = ''
+
 switch (releaseTarget) {
+  case 'dev-mirrorMedia':
+    assetPrefixPath = `https://dev.mirrormedia.mg/projects/${projectName}`
+    jsonEndpoint = 'https://whoareyou-gcs.readr.tw/json/2024homepage.json'
+    staticFileDestination = `https://dev.mirrormedia.mg/projects/${projectName}`
+    break
+  case 'dev-mirrorTV':
+    assetPrefixPath = `https://dev.mnews.tw/projects/${projectName}`
+    jsonEndpoint = 'https://whoareyou-gcs.readr.tw/json/2024homepage.json'
+    staticFileDestination = `https://dev.mnews.tw/projects/${projectName}`
+    break
+  case 'dev-readr':
+    assetPrefixPath = `https://dev.readr.tw/projects/3/${projectName}`
+    jsonEndpoint =
+      'https://storage.googleapis.com/whoareyou-gcs.readr.tw/json/2024cec_homepage.json'
+    staticFileDestination = `https://dev.readr.tw/projects/3/${projectName}`
+
+    break
   case 'mirrorMedia':
     assetPrefixPath = `https://www.mirrormedia.mg/projects/${projectName}`
     jsonEndpoint = 'https://whoareyou-gcs.readr.tw/json/2024homepage.json'
     staticFileDestination = `https://www.mirrormedia.mg/projects/${projectName}`
+
     break
   case 'mirrorTV':
     assetPrefixPath = `https://www.mnews.tw/projects/${projectName}`
@@ -18,8 +37,10 @@ switch (releaseTarget) {
     break
   case 'readr':
     assetPrefixPath = `https://www.readr.tw/projects/3/${projectName}`
-    jsonEndpoint = 'https://whoareyou-gcs.readr.tw/json/2024homepage.json'
+    jsonEndpoint =
+      'https://storage.googleapis.com/whoareyou-gcs.readr.tw/json/2024cec_homepage.json'
     staticFileDestination = `https://www.readr.tw/projects/3/${projectName}`
+
     break
   default:
     assetPrefixPath = '.'
