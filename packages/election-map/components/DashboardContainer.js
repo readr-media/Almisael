f49fef4df5a82c9bf590571e9e7d77c5ec358b85
caesar <caesar.lee@mirrormedia.mg>
2023-12-28 17:04:27 +0800

    refactor(election-map): store rwd device info in redux

--- packages/election-map/components/DashboardContainer.js
+++ packages/election-map/components/DashboardContainer.js
@@ -1,10 +1,12 @@
-import { forwardRef, useCallback, useRef, useState } from 'react'
+import { forwardRef, useCallback, useEffect, useRef, useState } from 'react'
 import { useElectionData } from '../hook/useElectinData'
 import { SpinningModal } from './SpinningModal'
 import useWindowDimensions from '../hook/useWindowDimensions'
 // import { MobileDashboard } from './MobileDashboard'
 import { MobileDashboardNew } from './mobile/MobileDashboardNew'
 import { Dashboard } from './Dashboard'
+import { useAppDispatch } from '../hook/useRedux'
+import { uiActions } from '../store/ui-slice'
 
 export const DashboardContainer = forwardRef(function DashboardContainer(
   { hasAnchor, dashboardInView },
@@ -14,6 +16,7 @@ export const DashboardContainer = forwardRef(function DashboardContainer(
   const loadingTimout = useRef(null)
   const { width } = useWindowDimensions()
   const isMobile = width <= 1024
+  const dispatch = useAppDispatch()
 
   const showLoading = useCallback((show) => {
     if (show) {
@@ -30,6 +33,14 @@ export const DashboardContainer = forwardRef(function DashboardContainer(
   }, [])
 
   const { onEvcSelected } = useElectionData(showLoading)
+
+  useEffect(() => {
+    if (width > 1024) {
+      dispatch(uiActions.changeUiDevice('desktop'))
+    } else {
+      dispatch(uiActions.changeUiDevice('mobile'))
+    }
+  }, [dispatch, width])
 
   if (!isMobile) {
     return (
--- packages/election-map/store/index.js
+++ packages/election-map/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import electionReducer from './election-slice'
 import mapReducer from './map-slice'
+import uiReducer from './ui-slice'
 
 /**
  * @typedef {ReturnType<typeof store.getState>} RootState
@@ -11,7 +12,7 @@ import mapReducer from './map-slice'
  */
 
 const store = configureStore({
-  reducer: { election: electionReducer, map: mapReducer },
+  reducer: { election: electionReducer, map: mapReducer, ui: uiReducer },
 })
 
 export default store
