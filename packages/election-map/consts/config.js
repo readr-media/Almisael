6a494590f3b89de8a8a80c595e5d30931a297f4f
caesar <caesar.lee@mirrormedia.mg>
2023-12-28 19:23:47 +0800

    refactor(election-map): update og image and gsutil command

--- packages/election-map/consts/config.js
+++ packages/election-map/consts/config.js
@@ -30,7 +30,9 @@ export const og = {
       : '鏡週刊即時關注 2024 年總統、立法委員結果！並且提供最詳盡的選舉票數地圖、歷年比較功能。',
   image:
     // 待更新
-    organization === 'readr-media' ? '' : '',
+    organization === 'readr-media'
+      ? 'https://v3-statics.mirrormedia.mg/images/16d67652-9355-4a47-b2da-e3b8801802f9-w1600.png'
+      : 'https://v3-statics.mirrormedia.mg/images/669355dc-9e08-456e-af3c-19b0dc69a600-w1600.png',
   url:
     organization === 'readr-media'
       ? 'https://www.mirrormedia.mg/projects/election2023/index.html'
@@ -38,6 +40,8 @@ export const og = {
 }
 
 /*
-gsutil -m cp -r -a public-read ./out/* gs://statics.mirrormedia.mg/projects/election2022
+gsutil -m cp -r ./out/* gs://v3-statics.mirrormedia.mg/projects/election2022
+gsutil -h "Cache-Control:no-store" -m cp -r ./out/* gs://v3-statics.mirrormedia.mg/projects/dev-election2024
 gsutil -m cp -r ./out/* gs://readr-coverage/project/3/election2022
+gsutil -h "Cache-Control:no-store" -m cp -r ./out/* gs://readr-coverage/project/3/dev-election2024
 */
