dc86eceac86a6701fe7fdd1fe8247f8201077c49
DianYangFu <dianyangfu@gmail.com>
2023-12-29 18:26:30 +0800

    feat(election-2024/config): add variables `breakpoint`

--- packages/election-2024/config/index.mjs
+++ packages/election-2024/config/index.mjs
@@ -1,10 +1,11 @@
 // 'mirrorMedia' | 'mirrorTV' | 'readr'
 const releaseTarget = process.env.NEXT_PUBLIC_RELEASE_TARGET
-const projectName = 'dev-election2024-homepage-1227-8'
+const projectName = 'dev-election2024-homepage-1229-5'
 let assetPrefixPath = ''
 let jsonEndpoint = ''
 let staticFileDestination = ''
 let watchMoreLinkSrc = ''
+let breakpoint = '1200px'
 switch (releaseTarget) {
   case 'dev-mirrorMedia':
     assetPrefixPath = `https://dev.mirrormedia.mg/projects/${projectName}`
@@ -12,6 +13,7 @@ switch (releaseTarget) {
     staticFileDestination = `https://dev.mirrormedia.mg/projects/${projectName}`
     watchMoreLinkSrc =
       'https://www.mirrormedia.mg/projects/election2024/index.html?utm_source=mmweb&utm_medium=dev_election2024'
+    breakpoint = '1200px'
     break
 
   case 'mirrorMedia':
@@ -20,6 +22,7 @@ switch (releaseTarget) {
     staticFileDestination = `https://www.mirrormedia.mg/projects/${projectName}`
     watchMoreLinkSrc =
       'https://www.mirrormedia.mg/projects/election2024/index.html?utm_source=mmweb&utm_medium=election2024'
+    breakpoint = '1200px'
     break
 
   case 'dev-mirrorTV':
@@ -28,6 +31,7 @@ switch (releaseTarget) {
     staticFileDestination = `https://dev.mnews.tw/projects/${projectName}`
     watchMoreLinkSrc =
       'https://dev.mnews.tw/projects/dev-election2024/index.html?utm_source=homepage&utm_medium=election2024'
+    breakpoint = '1127px'
     break
 
   case 'mirrorTV':
@@ -36,15 +40,16 @@ switch (releaseTarget) {
     staticFileDestination = `https://www.mnews.tw/projects/${projectName}`
     watchMoreLinkSrc =
       'https://www.mnews.tw/projects/election2024/index.html?utm_source=homepage&utm_medium=election2024'
+    breakpoint = '1127px'
 
     break
   case 'readr':
-    assetPrefixPath = `https://www.readr.tw/projects/3/${projectName}`
-    jsonEndpoint =
-      'https://storage.googleapis.com/whoareyou-gcs.readr.tw/json/2024cec_homepage.json'
-    staticFileDestination = `https://www.readr.tw/projects/3/${projectName}`
+    assetPrefixPath = `https://www.readr.tw/project/3/${projectName}`
+    jsonEndpoint = 'https://whoareyou-gcs.readr.tw/json/2024cec_homepage.json'
+    staticFileDestination = `https://www.readr.tw/project/3/${projectName}`
     watchMoreLinkSrc =
       'https://www.readr.tw/project/3/election2024/index.html?utm_source=READr&utm_medium=election2024'
+    breakpoint = '1200px'
 
     break
   default:
@@ -52,6 +57,8 @@ switch (releaseTarget) {
     jsonEndpoint = 'https://whoareyou-gcs.readr.tw/json/2024homepage.json'
     staticFileDestination = `http://localhost:3000`
     watchMoreLinkSrc = '.'
+    breakpoint = '1200px'
+
     break
 }
 
@@ -61,4 +68,5 @@ export {
   staticFileDestination,
   projectName,
   watchMoreLinkSrc,
+  breakpoint,
 }
