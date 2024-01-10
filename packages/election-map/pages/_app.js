610f42387fe86d80dca13034400765754e92c630
caesar <caesar.lee@mirrormedia.mg>
2024-01-10 16:00:25 +0800

    refactor(election-map): add comscore

--- packages/election-map/pages/_app.js
+++ packages/election-map/pages/_app.js
@@ -11,6 +11,19 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     gtag.init()
   }, [])
+
+  useEffect(() => {
+    // add comscore to track use traffic
+    var _comscore = _comscore || []
+    _comscore.push({ c1: '2', c2: '24318560' })
+    ;(function () {
+      var s = document.createElement('script'),
+        el = document.getElementsByTagName('script')[0]
+      s.async = true
+      s.src = 'https://sb.scorecardresearch.com/cs/24318560/beacon.js'
+      el.parentNode.insertBefore(s, el)
+    })()
+  }, [])
   return (
     <>
       <GlobalStyles />
