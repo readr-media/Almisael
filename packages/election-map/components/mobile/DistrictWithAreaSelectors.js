340c2c4b0000a8dcbcbe443df7fbfd3d0ef3e22b
LinHeMa <109965534+LinHeMa@users.noreply.github.com>
2025-07-21 12:00:44 +0800

    fix: build error

--- packages/election-map/components/mobile/DistrictWithAreaSelectors.js
+++ packages/election-map/components/mobile/DistrictWithAreaSelectors.js
@@ -180,6 +180,7 @@ export default function DistrictWithAreaSelectors({}) {
       ]
     }
     return [...allVillage]
+  }, [currentAreaCode])
 
   useEffect(() => {
     if (!hasDistrictMapping) {
