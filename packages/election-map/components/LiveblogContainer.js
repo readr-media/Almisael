0e373aaf275d9249bcca4b4a64b6a5ee48d5e25d
LinHeMa <109965534+LinHeMa@users.noreply.github.com>
2025-07-25 21:59:00 +0800

    feat: update liveblog

--- packages/election-map/components/LiveblogContainer.js
+++ packages/election-map/components/LiveblogContainer.js
@@ -58,7 +58,7 @@ export const LiveblogContainer = () => {
   const liveblogUrl =
     environment === 'dev'
       ? 'https://editools-gcs.readr.tw/files/liveblogs/taiwan-elections-test.json'
-      : 'https://editools-gcs.readr.tw/files/liveblogs/taiwan-elections.json'
+      : 'https://editools-gcs.readr.tw/files/liveblogs/legislator_11.json'
   useEffect(() => {
     const fetchLiveblog = async () => {
       const { data } = await axios.get(liveblogUrl)
